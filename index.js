549bc61e3e704dd7572fa8edd361cd91d2da6f87
Horizonte Deploy <deploy@horizonte.com>
2026-03-05 11:48:18 -0300

    q

--- index.js
+++
@@ -1,6 +0,0 @@
-// ============================================================
-// LEGEND: Este script pertence ao "Horizonte Financeiro"
-// LEGEND (PT): Ponto de entrada principal. Re-exporta o app do server.js.
-// ============================================================
-const app = require('./server');
-module.exports = app;
